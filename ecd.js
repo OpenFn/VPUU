20fb904f7cfa72290844cb5e6c243e7b8f341a84
Taylor Downs <taylor@openfn.org>
2017-07-14 12:33:35 -0400

    Update ecd.js

--- ecd.js
+++ ecd.js
@@ -50,7 +50,7 @@ each(
       )),
       upsert("Beneficiary_Attendance__c", "Unique_ID__c", fields(
         field("Unique_ID__c", function(state) {
-          console.log(state.data)
+          console.log(state)
           return `x${state.data.parentId}`.concat(state.data.child_scan)
         }),
         field("Attended_Left__c", function(state) {
